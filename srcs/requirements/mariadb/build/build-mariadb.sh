a4585aa8ed188f9af902cbd27192cf1a4fc16d67
Oscar <osc.boutarfa@gmail.com>
2023-05-15 23:57:42 +0200

    [finish volumes config and then push]

--- srcs/requirements/mariadb/build/build-mariadb.sh
+++ srcs/requirements/mariadb/build/build-mariadb.sh
@@ -1,1 +1,15 @@
 #!/bin/sh
+
+service mysql start 
+until mysqladmin ping >/dev/null 2>&1; do
+    sleep 1
+done
+
+echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
+echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mysql
+echo "FLUSH PRIVILEGES;" | mysql
+echo "CREATE DATABASE $MYSQL_DATABASE;" | mysql
+kill $(cat /var/run/mysqld/mysqld.pid)
+
+mysqld
