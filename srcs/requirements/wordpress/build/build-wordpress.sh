4c6b8bea576e7a6ea3d8fd6233b4d7a0954ff7b9
Oscar <osc.boutarfa@gmail.com>
2023-05-19 01:11:59 +0200

    [finish checking mariadb user creation]

--- srcs/requirements/wordpress/build/build-wordpress.sh
+++ srcs/requirements/wordpress/build/build-wordpress.sh
@@ -6,8 +6,8 @@ then
 else
     wp core download --allow-root \
     && wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOSTNAME --allow-root \
-    && wp core install --url=https://localhost --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWD --admin_email=$WP_ADMIN_MAIL --allow-root
-
+    && wp core install --url=https://localhost --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWD --admin_email=$WP_ADMIN_MAIL --allow-root \
+    && wp user create bob bob@example.com --role=author --porcelain --allow-root
 fi
 
 /usr/sbin/php-fpm7.3 -F -R
