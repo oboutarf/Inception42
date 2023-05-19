385db8827fb86fd7d0535ddabda6b44e2025d8f1
BOUTARFA Oscar <oscobou@MacBook-Pro-de-BOUTARFA.local>
2023-05-19 13:54:04 +0200

    [good init of database, last checks]

--- srcs/requirements/mariadb/build/build-mariadb.sh
+++ srcs/requirements/mariadb/build/build-mariadb.sh
@@ -2,17 +2,27 @@
 
 service mysql start
 
-if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
-then
-	echo "Database already exists"
+if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
+    echo "Database already exists"
 else
-    echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
-    echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
-    echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mysql
-    echo "FLUSH PRIVILEGES;" | mysql
+
     echo "CREATE DATABASE $MYSQL_DATABASE;" | mysql
+    echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
+    echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" | mysql
+    echo "FLUSH PRIVILEGES;" | mysql
+
+    mysql_secure_installation <<EOF
+
+Y
+$MYSQL_ROOT_PASSWORD
+$MYSQL_ROOT_PASSWORD
+Y
+Y
+Y
+Y
+EOF
+
 fi
 
-kill $(cat /var/run/mysqld/mysqld.pid)
-
+service mysql stop
 mysqld
--- srcs/requirements/wordpress/build/build-wordpress.sh
+++ srcs/requirements/wordpress/build/build-wordpress.sh
@@ -7,7 +7,7 @@ else
     wp core download --allow-root \
     && wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOSTNAME --allow-root \
     && wp core install --url=https://localhost --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWD --admin_email=$WP_ADMIN_MAIL --allow-root \
-    && wp user create bob bob@example.com --role=author --porcelain --allow-root
+    && wp user create $WP_USER $WP_USER_MAIL --role=author --user_pass=$WP_USER_PASSWD --porcelain --allow-root
 fi
 
 /usr/sbin/php-fpm7.3 -F -R
