1fe2d42685247b49ed784b2fc7a3cc435c542243
Oscar <osc.boutarfa@gmail.com>
2023-05-20 23:29:24 +0200

    [ok]

--- srcs/requirements/mariadb/conf/init-mariadb.sh
+++ srcs/requirements/mariadb/conf/init-mariadb.sh
@@ -7,7 +7,6 @@ echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD
 echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ;" >> db1.sql
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
-
 mysql < db1.sql
 
 kill $(cat /var/run/mysqld/mysqld.pid)
